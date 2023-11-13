46ede9fc1b1af0f912a17e1837a1fd45485f5275
Eunju Kim <dfiow@naver.com>
2023-11-13 21:20:13 +0900

    webB

---
+++ javascript/script.js
@@ -0,0 +1,48 @@
+$(function(){
+
+    // 메뉴 시작
+    $(".nav > ul > li").mouseover(function(){
+        $(this).find(".submenu").stop().slideDown();
+    });
+    $(".nav > ul > li").mouseout(function(){
+        $(this).find(".submenu").stop().slideUp();
+    });
+    // 메뉴 끝
+
+    // 슬라이드 시작
+    let currentindex = 0;
+
+    setInterval(function(){
+        let nextindex = (currentindex + 1) % 3;
+
+        $(".sliding").eq(currentindex).fadeOut(1200);
+        $(".sliding").eq(nextindex).fadeIn(1200);
+
+        currentindex=nextindex;
+    }, 3000);
+    // 슬라이드 끝
+
+    // 탭 시작
+    let tabBtn = $(".tab-btn > ul > li");
+    let tabCont = $(".tab-cont > div");
+
+    tabCont.hide().eq(0).show();
+
+    tabBtn.click(function(){
+        const index = $(this).index();
+
+        $(this).addClass("active").siblings().removeClass("active");
+        tabCont.eq(index).show().siblings().hide();
+    });
+    // 탭 끝
+
+    // 모달 레이어 팝업창 시작
+    $(".popup-btn").click(function(){
+        $(".popup-review").show();
+    });
+    $(".popup-close").click(function(){
+        $(".popup-review").hide();
+    });
+    // 모달 레이어 팝업창 끝
+
+});
